d057b7908e4646275a25e26143d9eaa529a58549
Chris Barrett <chris@walrus.cool>
2025-08-18 20:11:57 +1200

    Add wrapper configuration options for Perfect Dark

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -2,47 +2,93 @@
 
 let
   cfg = config.programs.perfectdark;
-
-  wrappedPerfectDark = pkgs.symlinkJoin {
-    name = "perfectdark-wrapped";
-    paths = [ cfg.package ];
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    postBuild =
-      if pkgs.stdenv.isDarwin then ''
-        wrapProgram $out/Applications/PerfectDark.app/Contents/MacOS/pd \
-          --add-flags --basedir='${cfg.baseDirectory}'
-      '' else ''
-        wrapProgram $out/bin/pd \
-          --add-flags --basedir='${cfg.baseDirectory}'
-      '';
-  };
 in
 {
-  options.programs.perfectdark = {
-    enable = lib.mkEnableOption "Perfect Dark port";
+  options.programs.perfectdark =
+    let
+      inherit (lib) types;
+    in
+    {
+      enable = lib.mkEnableOption "Perfect Dark port";
 
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.perfectdark;
-      defaultText = lib.literalExpression "pkgs.perfectdark";
-      description = "Perfect Dark package to use";
-    };
+      package = lib.mkOption {
+        type = types.package;
+        default = pkgs.perfectdark;
+        defaultText = lib.literalExpression "pkgs.perfectdark";
+        description = "Perfect Dark package to use";
+      };
 
-    baseDirectory = lib.mkOption {
-      type = lib.types.str;
-      default =
-        if pkgs.stdenv.isDarwin
-        then "${config.home.homeDirectory}/Library/Application Support/perfectdark"
-        else "${config.home.homeDirectory}/.local/share/perfectdark";
-      description = "Directory for Perfect Dark's runtime configuration and data dirs";
+      baseDirectory = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "Directory for Perfect Dark's runtime configuration and data dirs";
+      };
+
+      saveDirectory = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "Override save directory path";
+      };
+
+      modDirectory = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "Specify mod directory name/path";
+      };
+
+      romFile = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "Override ROM file name/path";
+      };
+
+      eepromFile = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "Override EEPROM save file name/path";
+      };
+
+      profile = lib.mkOption {
+        type = types.nullOr types.int;
+        default = null;
+        description = "Automatically select player file";
+      };
+
+      skipIntro = lib.mkOption {
+        type = types.bool;
+        default = false;
+        description = "Skip intro sequence";
+      };
     };
-  };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ wrappedPerfectDark ];
+    home.packages = [
+      (pkgs.symlinkJoin {
+        name = "perfectdark-wrapped";
+        paths = [ cfg.package ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild =
+          let
+            programPath =
+              if pkgs.stdenv.isDarwin then
+                "/Applications/PerfectDark.app/Contents/MacOS/pd"
+              else
+                "/bin/pd";
 
-    home.activation.perfectdarkDataDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p "${cfg.baseDirectory}/data"
-    '';
+            args = lib.cli.toGNUCommandLineShell { } {
+              basedir = cfg.baseDirectory;
+              savedir = cfg.saveDirectory;
+              moddir = cfg.modDirectory;
+              rom-file = cfg.romFile;
+              eeprom-file = cfg.eepromFile;
+              profile = cfg.profile;
+              skip-intro = cfg.skipIntro;
+            };
+          in
+          ''
+            wrapProgram $out/${programPath} --add-flags ${args}
+          '';
+      })
+    ];
   };
 }
